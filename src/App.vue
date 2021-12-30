b3e1c6609bcaa09ee0ee7374e2615591043ae637
JohnDennehy <johndennehy101@gmail.com>
2021-12-30 13:19:41 +0000

    Desktop skeleton styling in place for header component.

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,33 @@
 <template>
+  <Header />
   <router-view />
 </template>
 
 <script>
+import Header from "./components/Header.vue";
+
 export default {
   name: "App",
-  components: {},
+  components: {
+    Header,
+  },
 };
 </script>
 
 <style lang="scss">
-@import url("https://fonts.googleapis.com/css2?family=Open+Sans&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Great+Vibes&family=Open+Sans&display=swap");
 
 #app {
   font-family: "Open Sans", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+  padding: 0;
   background-color: grey;
+}
+* {
+  margin: 0;
+  padding: 0;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,86 @@
+<template>
+  <header v-if="loggedInHeader"></header>
+  <header v-else>
+    <div class="parent-container">
+      <div class="logo-and-title-container">
+        <img src="../assets/logoIcon.png" />
+        <p>Group Activity Planner</p>
+      </div>
+      <ul>
+        <li>Heading 1</li>
+        <li>Heading 2</li>
+        <li>Heading 3</li>
+        <li>Heading 4</li>
+      </ul>
+    </div>
+    <ul>
+      <li class="login">Login</li>
+      <li class="register">Register</li>
+    </ul>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      loggedInHeader: false,
+    };
+  },
+  watch: {
+    $route() {
+      if (this.$route.path !== "/") {
+        this.loggedInHeader = true;
+      }
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+header {
+  display: flex;
+  justify-content: space-between;
+
+  margin: 0px 0 40px 0;
+  padding: 30px 100px;
+  background-color: $header-background-colour;
+  color: #ffffff;
+}
+p {
+  font-weight: bold;
+  font-family: "Great Vibes", cursive;
+  font-size: 1.5rem;
+  margin-left: 1rem;
+  width: 100%;
+}
+img {
+  width: 3rem;
+  height: 2rem;
+}
+.parent-container {
+  display: flex;
+}
+.logo-and-title-container {
+  display: flex;
+  align-items: center;
+}
+ul {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-left: 3rem;
+
+  li {
+    list-style-type: none;
+    margin: 0 1.5rem;
+    width: 100%;
+    font-size: 1rem;
+  }
+}
+.login {
+  padding: 0.5rem 1rem;
+  border: 1px solid white;
+  border-radius: 50px;
+}
+</style>
