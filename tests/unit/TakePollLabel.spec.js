9f1f974d91e30e02e3d01ff46c010fe65cb654c7
JohnDennehy <johndennehy101@gmail.com>
2022-03-13 20:33:34 +0000

    Added tests for TakePollLabel component.

---
+++ tests/unit/TakePollLabel.spec.js
@@ -0,0 +1,36 @@
+import { mount, shallowMount } from "@vue/test-utils";
+import TakePollLabel from "@/components/TakePollLabel.vue";
+
+describe("TakePollLabel.vue", () => {
+  afterEach(() => {
+        jest.resetAllMocks();
+    });
+  it("should render correctly", () => {
+    const wrapper = shallowMount(TakePollLabel);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it("checks that poll option dates are rendered correctly", async () => {
+    const wrapper = mount(TakePollLabel, {
+      props: {
+        start: new Date(),
+        end: new Date(),
+        id: "1"
+      },
+    });
+
+    expect(wrapper.find("label")).toBeDefined();
+  });
+
+  it("checks computed properties are defined", async () => {
+    const wrapper = mount(TakePollLabel, {
+       props: {
+        startDate: new Date(),
+        endDate: new Date(),
+        id: "1"
+      },
+    });
+    expect(TakePollLabel.computed.formattedStartDate()).toBeDefined();
+    expect(TakePollLabel.computed.formattedEndDate()).toBeDefined();
+  });
+});
